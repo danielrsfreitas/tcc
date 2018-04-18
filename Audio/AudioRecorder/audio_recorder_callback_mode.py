51b49630e12d2f2699f0473eeb1974f63de6c586
Daniel Freitas <danielfreitas@Daniels-MacBook-Air.local>
2018-04-17 22:27:34 -0300

    Version_02

---
+++ Audio/AudioRecorder/audio_recorder_callback_mode.py
@@ -0,0 +1,58 @@
+"""PyAudio Example: Play a wave file."""
+
+import pyaudio
+import wave	
+import sys
+import time
+
+CHUNK = 1024 * 20 # Buffer size
+CHANNELS = 1 # Mono input
+FORMAT = pyaudio.paInt16 # Format
+RATE = 44100 # Sampling rate 
+
+# Define callback
+def callback(in_data, frame_count, time_info, status):
+	wf.writeframesraw(in_data) # Not so sure
+	return (in_data, pyaudio.paContinue)
+
+
+
+if len(sys.argv) < 2:
+    print("Records a wave file named %s \n\n" % sys.argv[0])
+    sys.exit(-1)
+
+wf = wave.open(sys.argv[1], 'w') # Returns a Wave_write object named 
+
+# Fill object parameters
+wf.setnchannels(CHANNELS) 
+wf.setsampwidth(2) # why 2 ? look for it in pyaudios documentation
+wf.setframerate(RATE)
+
+# instantiate PyAudio 
+p = pyaudio.PyAudio()
+
+# open stream 
+stream = p.open(
+				frames_per_buffer = CHUNK,
+	       		format = FORMAT,
+                channels = CHANNELS,
+                rate = RATE,
+                input = True,
+                stream_callback = callback)
+
+#data = stream.read(CHUNK)
+# Write acquired date in wave file
+
+stream.start_stream()
+
+while stream.is_active():
+	time.sleep(5)
+# stop stream 
+stream.stop_stream()
+stream.close()
+
+# Close wave file
+wf.close()
+
+# close PyAudio 
+p.terminate()
