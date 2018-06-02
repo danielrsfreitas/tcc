964dbf71b4e73100352afe32450424b190fcc3bd
Daniel Freitas <danielfreitas@Daniels-MacBook-Air.local>
2018-06-02 11:45:57 -0300

    Version_05

--- Audio/AudioRecorder/audio_recorder_callback_mode.py
+++ Audio/AudioRecorder/audio_recorder_callback_mode.py
@@ -1,45 +1,80 @@
 """
 Type in command line :
 
-	python3 audio_recorder_callback_mode.py audiofile_name.wav length_in_seconds levels_quant samp_rate
+	python3 audio_recorder_callback_mode.py audiofile_name.wav length_in_seconds levels_quant new_rate
 
 levels_quant : 1 - 8bits
 			   2 - 16bits 
 			   3 - 24bits
 			   4 - 32 bits
 
-samp_rate    : 44.1 kHz
-			   46   kHz 
-			   88.2 kHz
-			   96   kHz
+new_rate    :  Pick a new sapling rate, for instance
+			   44100   Hz
+			   46000   Hz 
+			   88200   Hz
+			   96000   Hz
 """
 
 import pyaudio
 import wave	
 import sys
 import time
+from scipy.io import wavfile
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 CHUNK = 1024 * 10 # Buffer size
 CHANNELS = 1 # Mono input
 FORMAT = {1 : pyaudio.paInt8, 2 : pyaudio.paInt16, 3 : pyaudio.paInt24, 4 : pyaudio.paInt32} # format options 
-RATE = [44100, 48000, 88200, 96000] # Maximum sampling rate microphone driver 
+RATE = 96000 # Maximum sampling rate microphone driver 
 SAMP_WIDTH = [1, 2, 3, 4]
 
 """
 
-Function to acquire audio in callback mode.
-	
-
 """
-def callback(in_data, frame_count, time_info, status):
-	wf.writeframesraw(in_data) 
-	return (in_data, pyaudio.paContinue)
+
+def sample_with_scipy(audio_name, audio_rate, new_rate):
+
+	# Ratio is the pace which the audio will be sampled in the numpy array
+	RATIO = round(audio_rate/new_rate)
+
+	# Opens the audio that was acquired with maximum sampling rate
+	rate, data = wavfile.read(audio_name)
+
+	# New numpy array with a lower sampling rate
+	data_sampled = data[::RATIO]
+
+	# Normalized from -1 to 1 
+	# Casting np array to float32 to carry more info (see scypi.io.wavfile.write doc.)
+	flt = data_sampled.astype(np.float32)/np.amax(data_sampled)
+
+	# Plot raw chart (96kHz)
+	plt.figure(1)
+	plt.plot(data)
+
+	# Plot sampled chart
+	plt.figure(2)
+	plt.plot(data_sampled)
+
+	# Plot -1 to 1 chart
+	plt.figure(3)
+	plt.plot(flt)
+
+	plt.show()
+
+	# Write audio files
+	wavfile.write('after_sample.wav',audio_rate, data)
+	wavfile.write('after_sample_32bits.wav',new_rate, flt)
+	wavfile.write('after_sample_worse.wav', new_rate, data_sampled)
+
+
 
 """
 
 	Did not work. Basically works as when we play the audio with a different sampling rate
 
-"""
+
 def sample_audio_44100(audio_name):
 	wf_read = wave.open(audio_name, 'r')
 	data = wf_read.readframes(wf_read.getnframes())
@@ -52,6 +87,19 @@ def sample_audio_44100(audio_name):
 
 	wf_read.close()
 	wf_write.close()
+"""
+
+
+"""
+
+Function to acquire audio in callback mode.
+	
+
+"""
+def callback(in_data, frame_count, time_info, status):
+	wf.writeframesraw(in_data) 
+	return (in_data, pyaudio.paContinue)
+
 
 """
 
@@ -65,16 +113,15 @@ def sample_audio_44100(audio_name):
 
 """
 
-def set_audiofile_params(audio_name, channels, samp_width, rate):
+def set_audiofile_params(audio_name, channels, samp_width, audio_rate):
 	wf = wave.open(audio_name, 'w') # Returns a wave_write object  
 
 	# Fill object parameters
 	wf.setnchannels(channels)
 	wf.setsampwidth(samp_width)
-	wf.setframerate(rate)
+	wf.setframerate(audio_rate)
 
 	return wf
-
 
 """
 	Main 
@@ -88,11 +135,11 @@ if len(sys.argv) < 5:
 audio_name = sys.argv[1]
 audio_len = sys.argv[2]
 audio_format = int(sys.argv[3])
-audio_rate = int(sys.argv[4])
+new_rate = int(sys.argv[4])
 
 
 
-wf = set_audiofile_params(audio_name, CHANNELS, audio_format, audio_rate) 
+wf = set_audiofile_params(audio_name, CHANNELS, audio_format, RATE) 
 # instantiate PyAudio 
 p = pyaudio.PyAudio()
 
@@ -101,7 +148,7 @@ stream = p.open(
 				frames_per_buffer = CHUNK,
 	       		format = int(FORMAT[audio_format]),
                 channels = CHANNELS,
-                rate = audio_rate,
+                rate = RATE,
                 input = True,
                 stream_callback = callback)
 
@@ -123,7 +170,7 @@ wf.close()
 # close PyAudio 
 p.terminate()
 
-#sample_audio_44100(audio_name)
+sample_with_scipy(audio_name, RATE, new_rate)
 
 
 
